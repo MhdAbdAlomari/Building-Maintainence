76370cb9ac09f896e9274d06c46bfdeb9b4c19fc
M-Abdulrhman-Alomari <abdulrhmangithub@gmail.com>
2026-05-24 09:24:20 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/create-admin', function () {
+    \App\Models\User::create([
+        'name' => 'Admin',
+        'email' => 'admin@shamfix.com',
+        'password' => bcrypt('password'),
+        'phone' => '0000000000',
+        'role' => 'admin',
+    ]);
+    return 'Admin created!';
+});
